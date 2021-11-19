53946da587147e835c1d22019ab058788e5c3ee9
Tyler Long <tyler4long@gmail.com>
2021-11-19 11:00:06 -0800

    Now we can display something in 3D

--- src/metaverse/scene.ts
+++ src/metaverse/scene.ts
@@ -42,11 +42,66 @@ class Scene {
     this.ground = createGround(this.square, this.scene);
     this.walls = createWalls(this.square, this.scene)!;
     this.player = createPlayer(this.square, this.scene);
+
+    this.keydownListener = this.keydownListener.bind(this);
+    window.addEventListener('keydown', this.keydownListener);
   }
 
-  render() {}
+  render() {
+    this.camera.position.x = this.player.position.x;
+    this.camera.position.z = this.player.position.z - 2;
+    this.camera.setTarget(this.player.position);
+    this.light.position.x = this.player.position.x;
+    this.light.position.z = this.player.position.z - 2;
+    this.scene.render();
+  }
 
-  dispose() {}
+  dispose() {
+    window.removeEventListener('keydown', this.keydownListener);
+    this.player.dispose();
+    this.camera.dispose();
+    this.light.dispose();
+    this.ground.dispose();
+    this.walls.dispose();
+    this.scene.dispose();
+  }
+
+  keydownListener(event: KeyboardEvent) {
+    const speed = 3;
+    switch (event.key) {
+      case 'ArrowLeft': {
+        event.preventDefault();
+        this.player.physicsImpostor?.setLinearVelocity(
+          new BABYLON.Vector3(-speed, 0, 0)
+        );
+        break;
+      }
+      case 'ArrowRight': {
+        event.preventDefault();
+        this.player.physicsImpostor?.setLinearVelocity(
+          new BABYLON.Vector3(speed, 0, 0)
+        );
+        break;
+      }
+      case 'ArrowUp': {
+        event.preventDefault();
+        this.player.physicsImpostor?.setLinearVelocity(
+          new BABYLON.Vector3(0, 0, speed)
+        );
+        break;
+      }
+      case 'ArrowDown': {
+        event.preventDefault();
+        this.player.physicsImpostor?.setLinearVelocity(
+          new BABYLON.Vector3(0, 0, -speed)
+        );
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
 }
 
 export default Scene;
