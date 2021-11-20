a1e0540d63b3c74b0b78b3ef7f83669418cc98bc
Tyler Long <tyler4long@gmail.com>
2021-11-20 09:59:46 -0800

    Use plane as screen

--- src/metaverse/meshes/screen.ts
+++ src/metaverse/meshes/screen.ts
@@ -4,45 +4,22 @@ import Square from '../square';
 import {uuid} from '../utils';
 
 export const createScreen = (square: Square, scene: BABYLON.Scene) => {
-  const columns = 6; // 6 columns
-  const rows = 4; // 4 rows
-
-  const faceUV = new Array(6);
-
-  //set all values to zero
-  for (let i = 0; i < 6; i++) {
-    faceUV[i] = new BABYLON.Vector4(0, 0, 0, 0);
-  }
-
-  //overwrite wanted face with sprite coordinates
-  faceUV[1] = new BABYLON.Vector4(3 / columns, 0, (3 + 1) / columns, 1 / rows);
-  const screen = BABYLON.MeshBuilder.CreateBox(
+  const screen = BABYLON.MeshBuilder.CreatePlane(
     uuid(),
-    {
-      width: 8,
-      height: 4.5,
-      depth: 1,
-      faceUV,
-    },
+    {width: 8, height: 4.5},
     scene
   );
-  screen.position = new BABYLON.Vector3(0, 5, (square.size - 1) / 2 - 1);
+  screen.rotate(BABYLON.Axis.Z, Math.PI, BABYLON.Space.WORLD);
+  screen.position = new BABYLON.Vector3(0, 4.5 / 2, (square.size - 1) / 2 - 1);
   const screenMaterial = new BABYLON.StandardMaterial(uuid(), scene);
   const videoTexture = new BABYLON.VideoTexture(
     uuid(),
     document.getElementById('video-1') as HTMLVideoElement,
     scene,
+    true,
     true
   );
   screenMaterial.diffuseTexture = videoTexture;
-  videoTexture.uScale = 8;
-  videoTexture.vScale = 3;
   screen.material = screenMaterial;
-  screen.physicsImpostor = new BABYLON.PhysicsImpostor(
-    screen,
-    BABYLON.PhysicsImpostor.BoxImpostor,
-    {mass: 1, friction: 1},
-    scene
-  );
   return screen;
 };
